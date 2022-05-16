3c357e6c45c9a7ea26a328a2f1a6f239cb4d4741
Sam2959 <l.shyamsukumar@gmail.com>
2022-05-16 23:42:50 +0530

    “Backend_Deployment”

--- static/__init__.py
+++ static/__init__.py
@@ -16,7 +16,7 @@ cors = CORS()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresq://zjvccdtdkhzcjd:98d42e9452daee73ebe4832200f54eb005af38b18a11d453392d39e6ac56893f@ec2-52-203-118-49.compute-1.amazonaws.com:5432/d2h1nbp38p4j0d'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zjvccdtdkhzcjd:98d42e9452daee73ebe4832200f54eb005af38b18a11d453392d39e6ac56893f@ec2-52-203-118-49.compute-1.amazonaws.com:5432/d2h1nbp38p4j0d'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
